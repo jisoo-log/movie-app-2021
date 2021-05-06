172a1c0c4164793915c903c40b6a001900b250d3
jisoolee <jisoolee.dev@gmail.com>
2021-05-06 20:14:42 +0900

    Add Movie genres

--- src/Movie.js
+++ src/Movie.js
@@ -1,11 +1,19 @@
 import React from "react";
 
-function Movie({ title, year, summary, poster }) {
+function Movie({ title, year, summary, poster, genres }) {
   return (
     <div class="movie">
       <img src={poster} alt={title} title={title} />
       <div class="movie__data">
         <h3 class="movie__title">{title}</h3>
+        <ul class="movie__genres">
+          {genres.map((genre, index) => (
+            <li key={index} class="genres__genre">
+              {genre}
+            </li>
+          ))}
+        </ul>
+        <h3 class="movie__genres">{genres}</h3>
         <h3 class="movie__year">{year}</h3>
         <h3 class="movie__summary">{summary}</h3>
       </div>
