de31f1458a91c2210a604c8e617831fc543e674f
jisoolee <jisoolee.dev@gmail.com>
2021-05-06 20:15:18 +0900

    Change class to className

--- src/Movie.js
+++ src/Movie.js
@@ -2,20 +2,20 @@ import React from "react";
 
 function Movie({ title, year, summary, poster, genres }) {
   return (
-    <div class="movie">
+    <div className="movie">
       <img src={poster} alt={title} title={title} />
-      <div class="movie__data">
-        <h3 class="movie__title">{title}</h3>
-        <ul class="movie__genres">
+      <div className="movie__data">
+        <h3 className="movie__title">{title}</h3>
+        <ul className="movie__genres">
           {genres.map((genre, index) => (
-            <li key={index} class="genres__genre">
+            <li key={index} className="genres__genre">
               {genre}
             </li>
           ))}
         </ul>
-        <h3 class="movie__genres">{genres}</h3>
-        <h3 class="movie__year">{year}</h3>
-        <h3 class="movie__summary">{summary}</h3>
+        <h3 className="movie__genres">{genres}</h3>
+        <h3 className="movie__year">{year}</h3>
+        <h3 className="movie__summary">{summary}</h3>
       </div>
     </div>
   );
