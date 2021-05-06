014f4842b338ba6339f3acdece89167c2a542fcf
jisoolee <jisoolee.dev@gmail.com>
2021-05-06 19:48:06 +0900

    Add basic component for Movie webpage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Movie from "./Movie";
+
 function App() {
-  return <div className="App" />;
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    const {
+      data: {
+        data: { movies: result },
+      },
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    setMovies(result);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return (
+    <div>
+      {isLoading
+        ? "Loading..."
+        : movies.map((movie) => {
+            return (
+              <Movie
+                key={movie.id}
+                title={movie.title}
+                year={movie.year}
+                summary={movie.summary}
+                poster={movie.poster}
+              />
+            );
+          })}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Movie.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Movie({ title, year, summary, poster }) {
+  return <h1>{title}</h1>;
+}
+
+export default Movie;
